03485675f4e55a4da3613947942e5165c9347a07
Alex <alex.hoch@gmx.de>
2020-01-06 10:35:42 +0100

    Create Function 'createScreen'

--- XcodeGitTut_doz_14/ViewController.swift
+++ XcodeGitTut_doz_14/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    func createScreen(){
+        print("create Buttons")
+    }
 
 }
 
