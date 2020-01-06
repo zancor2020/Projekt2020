be565c99e7077ef3970502a36f6e9fadb6be828e
Alex <alex.hoch@gmx.de>
2020-01-06 10:34:32 +0100

    Initial Commit

---
+++ XcodeGitTut_doz_14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGitTut_doz_14
+//
+//  Created by Alexander Hoch on 06.01.20.
+//  Copyright © 2020 zancor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XcodeGitTut_doz_14Tests/XcodeGitTut_doz_14Tests.swift
@@ -0,0 +1,34 @@
+//
+//  XcodeGitTut_doz_14Tests.swift
+//  XcodeGitTut_doz_14Tests
+//
+//  Created by Alexander Hoch on 06.01.20.
+//  Copyright © 2020 zancor. All rights reserved.
+//
+
+import XCTest
+@testable import XcodeGitTut_doz_14
+
+class XcodeGitTut_doz_14Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
